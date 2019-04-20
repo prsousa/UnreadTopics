e0b4bd3d8b3a035ebbaea830973979e40277024b
Paulo Sousa <prsousa34@gmail.com>
2019-04-20 19:12:31 +0100

    Uses HTTPS to register for push notifications

--- gcm.js
+++ gcm.js
@@ -1,17 +1,17 @@
-"use strict"
+"use strict";
 
 let GCMManager = {};
 
-GCMManager.pushRegistrationId = function (userId, registrationId) {
-    return Utils.ajax({
-        url: "http://topicosporler.tk/gcm.php",
-        method: 'POST',
-        data: { id_member: userId, registrationId: registrationId }
-    });
-}
+GCMManager.pushRegistrationId = function(userId, registrationId) {
+  return Utils.ajax({
+    url: "https://topicosporler.tk/gcm.php",
+    method: "POST",
+    data: { id_member: userId, registrationId: registrationId }
+  });
+};
 
-GCMManager.register = function (userId, fcmServerId) {
-    return chromep.gcm.register([fcmServerId]).then(registrationId => {
-        return GCMManager.pushRegistrationId(userId, registrationId);
-    });
-}
+GCMManager.register = function(userId, fcmServerId) {
+  return chromep.gcm.register([fcmServerId]).then(registrationId => {
+    return GCMManager.pushRegistrationId(userId, registrationId);
+  });
+};
